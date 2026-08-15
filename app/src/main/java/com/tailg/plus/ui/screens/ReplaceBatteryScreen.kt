9248730d133bb3b1529668d3a64e35b61da46d69
ch6vip <ch6vip@users.noreply.github.com>
2026-08-16 05:20:58 +0800

    fix(ui): import DatePicker instead of DatePickerDialog

--- app/src/main/java/com/tailg/plus/ui/screens/ReplaceBatteryScreen.kt
+++ app/src/main/java/com/tailg/plus/ui/screens/ReplaceBatteryScreen.kt
@@ -18,7 +18,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
